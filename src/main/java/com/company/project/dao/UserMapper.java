e537e891de8397f73b67d931b3522059549f1dfb
xiao907512682 <907512682@qq.com>
2021-03-23 17:03:31 +0800

    根据账号查用户

--- src/main/java/com/company/project/dao/UserMapper.java
+++ src/main/java/com/company/project/dao/UserMapper.java
@@ -2,6 +2,9 @@ package com.company.project.dao;
 
 import com.company.project.core.Mapper;
 import com.company.project.model.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends Mapper<User> {
+
+    User getUserByAccount(@Param("account")String account);
 }
